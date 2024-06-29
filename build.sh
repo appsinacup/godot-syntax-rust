2b63afd7f32e400ae748922ca9b28ce77ed475ce
Dragos Daian <daian.dragos@yahoo.com>
2024-06-29 11:11:22 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cargo fmt -- --config-path rustfmt.toml
 cargo clippy --fix --allow-dirty
 cargo build
-mv target/debug/librust_syntax.dylib demo/addons/rust-syntax/bin/librust-syntax.macos.template_debug.dylib
+cp target/debug/librust_syntax.dylib demo/addons/rust-syntax/bin/librust-syntax.macos.template_debug.dylib
